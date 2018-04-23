803e8eac9f1b00ae5ad5aacc1d5695a51e84079b
JohnCdf <diaz.john312@gmail.com>
2018-04-22 20:03:37 -0400

    Added menu animation

--- src/script.js
+++ src/script.js
@@ -1,6 +1,43 @@
+function menuAnimation() {
+    $("#nav-icon").toggleClass('active');
+    $(".global-nav").toggleClass('active');
+    $(".menu_nav").toggleClass('active');
+    $(".fa-shopping-bag").toggleClass('active');
+
+    var interval = 0;
+
+    var list = $(".menu_nav").children();
+
+    if($(list).children().hasClass('active')){
+        $($(list).children().get().reverse()).each(function(){
+            var element = $(this);
+    
+            setTimeout(function(){
+                element.toggleClass('active');
+            },interval);
+    
+            interval+=35;
+    
+        })
+    } else {
+        $(list).children().each(function(){
+            var element = $(this);
+    
+            setTimeout(function(){
+                element.toggleClass('active');
+            },interval);
+    
+            interval+=35;
+    
+        })
+    }
+}
 $("#nav-icon").click(function(){
-    $(this).toggleClass('active');
-    $(".menu").animate({
-        height: "toggle"
-    });
+    menuAnimation()
 });
+
+window.addEventListener('resize',function(){
+    if(window.innerWidth > 723 && $(".menu_nav").hasClass("active")){
+        menuAnimation()
+    }
+})
